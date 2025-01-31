2b0bbd7184bb076f2f56f3a69a3f108f0eb5d44a
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-01-31 13:59:58 -0800

    afternoon tasks jan 31

--- inc/post-types-taxonomies.php
+++ inc/post-types-taxonomies.php
@@ -145,6 +145,59 @@ function mindset_register_custom_post_types()
         )
     );
     register_post_type('fwd-job-posting', $args);
+
+    // Services CPT
+    $labels = array(
+        'name' => _x('Services', 'Post Type General Name', 'mindset-theme'),
+        'singular_name' => _x('Service', 'Post Type Singular Name', 'mindset-theme'),
+        'menu_name' => _x('Services', 'Admin Menu text', 'mindset-theme'),
+        'name_admin_bar' => _x('Service', 'Add New on Toolbar', 'mindset-theme'),
+        'archives' => __('Service Archives', 'mindset-theme'),
+        'attributes' => __('Service Attributes', 'mindset-theme'),
+        'parent_item_colon' => __('Parent Service:', 'mindset-theme'),
+        'all_items' => __('All Services', 'mindset-theme'),
+        'add_new_item' => __('Add New Service', 'mindset-theme'),
+        'add_new' => __('Add New', 'mindset-theme'),
+        'new_item' => __('New Service', 'mindset-theme'),
+        'edit_item' => __('Edit Service', 'mindset-theme'),
+        'update_item' => __('Update Service', 'mindset-theme'),
+        'view_item' => __('View Service', 'mindset-theme'),
+        'view_items' => __('View Services', 'mindset-theme'),
+        'search_items' => __('Search Service', 'mindset-theme'),
+        'not_found' => __('Not found', 'mindset-theme'),
+        'not_found_in_trash' => __('Not found in Trash', 'mindset-theme'),
+        'featured_image' => __('Featured Image', 'mindset-theme'),
+        'set_featured_image' => __('Set featured image', 'mindset-theme'),
+        'remove_featured_image' => __('Remove featured image', 'mindset-theme'),
+        'use_featured_image' => __('Use as featured image', 'mindset-theme'),
+        'insert_into_item' => __('Insert into Service', 'mindset-theme'),
+        'uploaded_to_this_item' => __('Uploaded to this Service', 'mindset-theme'),
+        'items_list' => __('Services list', 'mindset-theme'),
+        'items_list_navigation' => __('Services list navigation', 'mindset-theme'),
+        'filter_items_list' => __('Filter Services list', 'mindset-theme'),
+    );
+    $args = array(
+        'label' => __('Service', 'mindset-theme'),
+        'description' => __('List of services', 'mindset-theme'),
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-hammer',
+        'supports' => array('title', 'editor'),
+        'taxonomies' => array(),
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 20,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => false,
+        'hierarchical' => false,
+        'exclude_from_search' => false,
+        'show_in_rest' => true,
+        'publicly_queryable' => true,
+        'capability_type' => 'post',
+    );
+    register_post_type('service', $args);
 }
 add_action('init', 'mindset_register_custom_post_types');
 
--- mindset-blocks/mindset-blocks.php
+++ mindset-blocks/mindset-blocks.php
@@ -13,6 +13,9 @@ function mindset_blocks_mindset_blocks_block_init()
 	register_block_type(__DIR__ . '/build/copyright');
 	register_block_type(__DIR__ . '/build/company-address');
 	register_block_type(__DIR__ . '/build/company-email');
+	register_block_type(__DIR__ . '/build/service-posts', array(
+		'render_callback' => 'fwd_render_service_posts'
+	));
 }
 add_action('init', 'mindset_blocks_mindset_blocks_block_init');
 
@@ -43,3 +46,55 @@ function mindset_register_custom_fields()
 	);
 }
 add_action('init', 'mindset_register_custom_fields');
+
+// fwd render service posts
+function fwd_render_service_posts($attributes)
+{
+	ob_start();
+?>
+	<div <?php echo get_block_wrapper_attributes(); ?>>
+		<?php
+		// First WP_Query to output titles wrapped in <a> tags for in-page navigation
+		$args = array(
+			'post_type'      => 'service',
+			'posts_per_page' => -1,
+			'orderby'        => 'title',
+			'order'          => 'ASC',
+		);
+
+		$query = new WP_Query($args);
+
+		if ($query->have_posts()) {
+			while ($query->have_posts()) {
+				$query->the_post();
+		?>
+				<a href="#post-<?php the_ID(); ?>"><?php the_title(); ?></a><br>
+			<?php
+			}
+			wp_reset_postdata();
+		} else {
+			echo '<p>No posts found.</p>';
+		}
+
+		// Second WP_Query to output the title and content of each post
+		$query = new WP_Query($args);
+
+		if ($query->have_posts()) {
+			while ($query->have_posts()) {
+				$query->the_post();
+			?>
+				<div id="post-<?php the_ID(); ?>">
+					<h2><?php the_title(); ?></h2>
+					<div><?php the_content(); ?></div>
+				</div>
+		<?php
+			}
+			wp_reset_postdata();
+		} else {
+			echo '<p>No posts found.</p>';
+		}
+		?>
+	</div>
+<?php
+	return ob_get_clean();
+}
