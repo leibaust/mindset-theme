80a2d0e7470cea5870883ef290bb303d2a1726b0
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-02-04 12:46:52 -0800

    feb 4 morning lecture

---
+++ mindset-blocks/build/testimonial-slider/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bfd484bb81e316062a34');
---
+++ patterns/book-review.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Title: Book Review
+ * Slug: mindset-theme/book-review
+ * Category: media, text
+ */
+?>
+
+
+<!-- wp:media-text {"mediaId":143,"mediaLink":"http://localhost/cms/mindset/works/flowers-project/yellow-flowers/","mediaType":"image","mediaWidth":40,"mediaSizeSlug":"large"} -->
+<div class="wp-block-media-text is-stacked-on-mobile" style="grid-template-columns:40% auto">
+    <figure class="wp-block-media-text__media"><img src="http://localhost/cms/mindset/wp-content/uploads/2025/01/yellow-flowers-1024x682.jpg" alt="" class="wp-image-143 size-large" /></figure>
+    <div class="wp-block-media-text__content"><!-- wp:heading -->
+        <h2 class="wp-block-heading">Title</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p>Author</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph -->
+        <p>Description</p>
+        <!-- /wp:paragraph -->
+    </div>
+</div>
+<!-- /wp:media-text -->
